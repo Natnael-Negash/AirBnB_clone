91b6c07286af8a400c29293cd61a9245d823949a
Natnael-Negash <nnegash658@gmail.com>
2024-02-11 13:42:50 -0900

    updated

--- models/city.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defines a class City that is the template for city object"""
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """Class representing a city"""
-    state_id = ""
-    name = ""
